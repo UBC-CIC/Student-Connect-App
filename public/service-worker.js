77c74c7041a20e3e2f2ee2fc063a0440a42ca472
ecaiyonotbad <cyyedward@hotmail.com>
2021-07-28 16:07:17 -0700

    disable service worker, fixed account creation bug

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,41 +1,41 @@
-var cacheName = 'appCache';
-
-self.addEventListener("activate", event => {
-    const cacheKeeplist = [cacheName];
-    event.waitUntil(
-        caches.keys()
-            .then(keyList =>
-                Promise.all(keyList.map(key => {
-                    if (!cacheKeeplist.includes(key)) {
-                        return caches.delete(key);
-                    }
-                }))
-            )
-    );
-});
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(
-                [
-                    //Insert which files should be cached here
-
-                ]
-            );
-        })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.open(cacheName).then(function(cache) {
-            return cache.match(event.request).then(function (response) {
-                return response || fetch(event.request).then(function(response) {
-                    cache.put(event.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
-});
+// var cacheName = 'appCache';
+//
+// self.addEventListener("activate", event => {
+//     const cacheKeeplist = [cacheName];
+//     event.waitUntil(
+//         caches.keys()
+//             .then(keyList =>
+//                 Promise.all(keyList.map(key => {
+//                     if (!cacheKeeplist.includes(key)) {
+//                         return caches.delete(key);
+//                     }
+//                 }))
+//             )
+//     );
+// });
+//
+// self.addEventListener('install', function(event) {
+//     event.waitUntil(
+//         caches.open(cacheName).then(function(cache) {
+//             return cache.addAll(
+//                 [
+//                     //Insert which files should be cached here
+//
+//                 ]
+//             );
+//         })
+//     );
+// });
+//
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//         caches.open(cacheName).then(function(cache) {
+//             return cache.match(event.request).then(function (response) {
+//                 return response || fetch(event.request).then(function(response) {
+//                     cache.put(event.request, response.clone());
+//                     return response;
+//                 });
+//             });
+//         })
+//     );
+// });
--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -32,7 +32,7 @@ function Login(props) {
                     updateCurrentUser(user)
                     updateLoginState("signedIn");
                 }).catch(err => {
-                    updateLoginState("signIn");
+                    console.log(err)
                 })
 
             } catch (e) {
