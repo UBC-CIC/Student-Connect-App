78da69f95963f1725fddf198feb2c52fbb37d700
qlnjq <45474496+qlnjq@users.noreply.github.com>
2021-10-22 13:24:27 -0700

    Update ssm param names (part 2)

--- deployFrontend.sh
+++ deployFrontend.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export AthleticsNewsTableArn=$(aws ssm get-parameter --name AthleticsNewsTableArn --query Parameter.Value --output text)
-export BlogsTableArn=$(aws ssm get-parameter --name BlogsTableArn --query Parameter.Value --output text)
-export ClubsTableArn=$(aws ssm get-parameter --name ClubsTableArn --query Parameter.Value --output text)
-export EventsTableArn=$(aws ssm get-parameter --name EventsTableArn --query Parameter.Value --output text)
-export GetESDocumentsLambdaArn=$(aws ssm get-parameter --name GetESDocumentsLambdaArn --query Parameter.Value --output text)
-export NewsTableArn=$(aws ssm get-parameter --name NewsTableArn --query Parameter.Value --output text)
+PREFIX=/amplify/
+export AthleticsNewsTableArn=$(aws ssm get-parameter --name ${PREFIX}AthleticsNewsTableArn --query Parameter.Value --output text)
+export BlogsTableArn=$(aws ssm get-parameter --name ${PREFIX}BlogsTableArn --query Parameter.Value --output text)
+export ClubsTableArn=$(aws ssm get-parameter --name ${PREFIX}ClubsTableArn --query Parameter.Value --output text)
+export EventsTableArn=$(aws ssm get-parameter --name ${PREFIX}EventsTableArn --query Parameter.Value --output text)
+export GetESDocumentsLambdaArn=$(aws ssm get-parameter --name ${PREFIX}GetESDocumentsLambdaArn --query Parameter.Value --output text)
+export NewsTableArn=$(aws ssm get-parameter --name ${PREFIX}NewsTableArn --query Parameter.Value --output text)
 python makePolicy.py $AthleticsNewsTableArn $BlogsTableArn $ClubsTableArn $EventsTableArn $GetESDocumentsLambdaArn $NewsTableArn
