073827fcdeb23d9653a712e7d87a20a0d3c0f176
qlnjq <45474496+qlnjq@users.noreply.github.com>
2021-12-27 18:21:15 -0800

    Add policies to ddb and GetESDocumentsLambdaArn

--- makePolicy.py
+++ makePolicy.py
@@ -8,12 +8,17 @@ print(sys.argv)
 #GetESDocumentsLambdaArn=sys.argv[5]
 #NewsTableArn=sys.argv[6]
 
+GetESDocumentsLambdaArn=sys.argv[1]
+DocumentsTableArn=sys.argv[2]
+
+
 #arnList=[AthleticsNewsTableArn,BlogsTableArn,ClubsTableArn,EventsTableArn,GetESDocumentsLambdaArn,NewsTableArn]
+arnList=[GetESDocumentsLambdaArn,DocumentsTableArn]
 
 print(sys.argv)
 f = open('lambda-dynamodb-policy.json')
 data = json.load(f)
-#data["Statement"][0]["Resource"]=arnList
+data["Statement"][0]["Resource"]=arnList
 
 
 with open('lambda-dynamodb-policy.json', 'w') as f:
